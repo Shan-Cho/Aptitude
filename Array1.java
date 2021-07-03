43b662dc683f770e8f4345bc19c5f862317f9fdd
Shan-Cho <85380169+Shan-Cho@users.noreply.github.com>
2021-07-03 16:00:22 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,21 @@
+package com.ChouguleIndustries.aptitude;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		int n = 3;
+		for (int j = 1; j <= n; j++) {
+			for (int i = 1; i <= n; i++) {
+				if (i == 3) {
+					System.out.print(j + "" + i + " ");
+				} else if (j == 3) {
+					System.out.print(j + "" + i + " ");
+				} else {
+					System.out.print("   ");
+				}
+			}
+
+			System.out.println();
+		}
+	}
+}
